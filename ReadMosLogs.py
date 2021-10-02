af656db85a1ed869cb5c198626018bb29bf40a85
jingjingh20 <65928703+jingjingh20@users.noreply.github.com>
2021-10-01 21:14:39 -0400

    Update ReadMosLogs.py

--- ReadMosLogs.py
+++ ReadMosLogs.py
@@ -1,13 +1,13 @@
 import os
 
-filepath = 'C:/CGTN/805'
+filepath = 'C:/abc/805'
 files = [fn for fn in os.listdir(filepath)]
 # perhaps filter `files`
 string1 = '<roStatus>ERROR'
 string2 = 'Parse XML from ncs failed.'
 
 def does_fn_contain_string(filename):
-  with open('C:/CGTN/805/' + filename, "r", encoding="utf-8") as blargh:
+  with open('C:/abc/805/' + filename, "r", encoding="utf-8") as blargh:
     content = blargh.read()
     return string2 in content
 
